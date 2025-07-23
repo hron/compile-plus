c1c3653f89dd8da5bb4a95ca6f27e43b1520c1f0
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-07-23 10:30:35 +0300

    Use short names in the tests

--- test/compile-plus-tests.el
+++ test/compile-plus-tests.el
@@ -10,29 +10,29 @@
 (require 'compile-plus)
 (require 'ert)
 
-(defun compile-plus-tests--sample-provider ()
+(defun sample-provider ()
   "Sample provider that always return the same list."
   '("Sample #1" "Sample #2"))
 
-(defun compile-plus-tests--dont-match-provider ()
+(defun dont-match-provider ()
   "Sample provider that always return the same list."
   '("Don't match"))
 
-(ert-deftest compile-plus-tests--find-providers-for-major-mode ()
+(ert-deftest find-providers-for-major-mode ()
   (with-temp-buffer
     (let ((compile-plus-providers-alist
-           '((c-mode . (compile-plus-tests--sample-provider))
-             (text-mode . (compile-plus-tests--dont-match-provider)))))
+           '((c-mode . (sample-provider))
+             (text-mode . (dont-match-provider)))))
       (c-mode)
       (compile-plus-mode +1)
       (should (equal (compile-plus--build-future-history)
                      '("Sample #1" "Sample #2"))))))
 
-(ert-deftest compile-plus-tests--override-providers ()
+(ert-deftest override-providers ()
   (with-temp-buffer
     (let ((compile-plus-providers-alist
-           '((text-mode . (compile-plus-tests--dont-match-provider))))
-          (compile-plus-override-providers '(compile-plus-tests--sample-provider)))
+           '((text-mode . (dont-match-provider))))
+          (compile-plus-override-providers '(sample-provider)))
       (text-mode)
       (compile-plus-mode +1)
       (should (equal (compile-plus--build-future-history)
