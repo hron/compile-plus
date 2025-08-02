467d23986f9a70bc0d257d1b31d7fcf1c38a9374
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-08-02 12:13:13 +0300

    Add Emacs and eask to flake.nix

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-emacs-ci.url = "github:purcell/nix-emacs-ci";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-emacs-ci,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        mkEmacsEnv =
+          { emacs }:
+          pkgs.mkShell {
+            name = "emacs-${emacs.version}";
+            packages = [ emacs ] ++ [ pkgs.eask-cli ];
+
+            shellHook = ''
+              export PATH=${emacs}/bin:$PATH
+              export EMACSLOADPATH="$(pwd):$EMACSLOADPATH"
+
+              echo "--- Welcome to the Emacs ${emacs.version} development shell! ---"
+              # echo "Emacs path: $(which emacs)"
+              # echo "Emacs path: $(emacs --version)"
+              echo -e "\n> emacs --version"
+              emacs --version
+              echo -e "\n> eask --version"
+              eask --version
+            '';
+          };
+
+      in
+      {
+        devShells = {
+          emacs30 = mkEmacsEnv {
+            emacs = nix-emacs-ci.packages.${system}.emacs-30-1;
+          };
+          emacsSnapshot = mkEmacsEnv {
+            emacs = nix-emacs-ci.packages.${system}.emacs-snapshot;
+          };
+        };
+      }
+    );
+}
