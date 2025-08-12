e4f35cba2f9edf51817eb5a6ed129d8855aae88b
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-08-12 11:41:49 +0300

    Replace future history with `*-thing-at-point`

--- test/compile-plus-tests.el
+++ test/compile-plus-tests.el
@@ -15,50 +15,42 @@
 DEBUG"
   (if debug
       '(launch command "sample-command-1")
-    '("Sample #1")))
+    "Sample #1"))
 
 (defun sample-provider-2 (&optional debug)
   "Sample provider that always return the same list.
 DEBUG"
   (if debug
       '(launch command "sample-command-2")
-    '("Sample #2")))
+    "Sample #2"))
 
 (defun dont-match-provider (&optional debug)
   "Sample provider that always return the same list.
 If DEBUG is t then return dape-config."
   (if debug
       '(launch command "Don't match provider!")
-    '("Don't match")))
+    "Don't match"))
 
-(ert-deftest compile-future-history-for-major-mode ()
+(defun error-provider (&optional debug)
+  "Sample provider function that always produces an error.
+DEBUG has no meaning."
+  (error "Needed external file doesn't exist"))
+
+(ert-deftest compile-command-for-major-mode ()
   (with-temp-buffer
     (let ((compile-plus-providers-alist
            '((c-mode . (sample-provider-1 sample-provider-2))
              (text-mode . (dont-match-provider)))))
       (c-mode)
-      (compile-plus-mode +1)
-      (should (equal (compile-plus--build-future-history)
-                     '("Sample #1" "Sample #2"))))))
+      (should (equal (compile-plus-compile-command) "Sample #1")))))
 
-(ert-deftest compile-future-history-override-providers ()
-  (with-temp-buffer
-    (let ((compile-plus-providers-alist
-           '((text-mode . (dont-match-provider))))
-          (compile-plus-override-providers '(sample-provider-1 sample-provider-2)))
-      (text-mode)
-      (compile-plus-mode +1)
-      (should (equal (compile-plus--build-future-history)
-                     '("Sample #1" "Sample #2"))))))
-
-(ert-deftest compile-future-history-rescue-errors ()
+(ert-deftest compile-command-rescue-errors ()
   (with-temp-buffer
     (let ((debug-on-error nil)
           (compile-plus-providers-alist
-           '((text-mode . ((lambda () (error "Needed external file doesn't exist")))))))
+           '((text-mode . (error-provider)))))
       (text-mode)
-      (compile-plus-mode +1)
-      (should (equal (compile-plus--build-future-history)
+      (should (equal (compile-plus-compile-command)
                      '())))))
 
 (ert-deftest dape-command-for-major-mode ()
@@ -69,3 +61,11 @@ If DEBUG is t then return dape-config."
       (c-mode)
       (should (equal (compile-plus-dape-command)
                      '(launch command "sample-command-1"))))))
+
+(ert-deftest dape-command-rescue-errors ()
+  (with-temp-buffer
+    (let ((debug-on-error nil)
+          (compile-plus-providers-alist
+           '((text-mode . (error-provider)))))
+      (text-mode)
+      (should (equal (compile-plus-dape-command) '())))))
